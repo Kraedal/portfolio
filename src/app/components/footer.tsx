447dfb286880e85adc5295b28d9893b43bfd1978
kraedal <jacobong25@icloud.com>
2025-01-09 00:24:15 +0800

    Fix: Footer Spacing

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -9,12 +9,12 @@ export default function Footer() {
             </h1>
 
             <div className="divide-x-0 lg:divide-x-2 divide-white divide-opacity-30 grid grid-cols-1 lg:grid-cols-3 gap-8">
-                <div className="h-4  w-full px-8 py-2">
+                <div className="h-10  w-full px-8 py-2">
                     <h1 className="text-lg text-gray-500 mb-2 uppercase tracking-wide lg:tracking-widest">Contact me  <span className="ml-2">📧</span></h1>
                     <Link href="mailto:sorianojacobong@gmail.com">sorianojacobong@gmail.com</Link>
                 </div>
                 {/* Connect Div */}
-                <div className="h-4  w-full px-8 py-2">
+                <div className="h-10  w-full px-8 py-2">
                 <h1 className="text-lg  text-gray-500 mb-2 uppercase tracking-wide lg:tracking-widest">Let&apos;s Connect<span className="ml-2">🤝</span></h1>
                 <div className="flex flex-row gap-1 flex-wrap">
                 <Link
@@ -45,7 +45,7 @@ export default function Footer() {
                 </div>
                 </div>
                 {/* Last Section */}
-                <div className="h-4  w-full px-8 py-2">
+                <div className="h-10  w-full px-8 py-2">
                     <h1 className="text-lg text-gray-500 mb-2 uppercase tracking-wide lg:tracking-widest">Made using <span className="ml-2"></span>🍵</h1>
                     <p className=" ">NextJS + TailwindCSS + Motion</p>
                 </div>
